51d824317d2fe1c9863b1e4c13b848e23ce4a975
DashiellLumas <dashiell.lumas@gmail.com>
2018-08-10 23:27:44 -0400

    added email functionality

--- server/server.js
+++ server/server.js
@@ -8,3 +8,39 @@ app.set('view engine', 'ejs');
 
 app.use('/', express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/contact', function (req, res){
+  let mailOpts, smtpTrans;
+  var username = process.env.GMAIL_USERNAME,
+  password = process.env.GMAIL_PASSWORD;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: username,
+      pass: password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: username,
+    subject: 'New message from your personal website',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}
+    \n Phone Number:${req.body.phone}
+    `
+
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response){
+    if(error){
+      console.log(error);
+    }
+    else {
+      console.log('Message sent:' + response.response)
+      res.send(200);
+    }
+  });
+});
